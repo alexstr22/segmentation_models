a679ae5ba9e1db2b5883b2807c2f2e74a3783b17
alex_str <alexstr22@gmail.com>
2019-10-19 23:43:12 +0300

    CAM & PAM, test small size

--- segmentation_models/models/fpn.py
+++ segmentation_models/models/fpn.py
@@ -101,23 +101,24 @@ def FPNBlock(pyramid_filters,segmentation_filters, stage):
             name=conv1_name,
         )(skip)
 
-        if stage == 5:
+        if (stage == 5) or (stage == 4):
             # input_tensor
             pam = PAM()(input_tensor)
-            pam = Conv2D(segmentation_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
             pam = BatchNormalization(axis=3)(pam)
             pam = Activation('relu')(pam)
             pam = Dropout(0.5)(pam)
-            pam = Conv2D(segmentation_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
 
             input_tensor = pam
+
             # skip
-            pam = PAM()(skip)
-            pam = Conv2D(segmentation_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            pam = CAM()(skip)
+            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
             pam = BatchNormalization(axis=3)(pam)
             pam = Activation('relu')(pam)
             pam = Dropout(0.5)(pam)
-            pam = Conv2D(segmentation_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
 
             skip = pam
 
@@ -141,8 +142,8 @@ def build_fpn(
         backbone,
         attention,
         skip_connection_layers,
-        pyramid_filters=256,
-        segmentation_filters=128,
+        pyramid_filters=64,#original 256
+        segmentation_filters=32, # original 128
         classes=1,
         activation='sigmoid',
         use_batchnorm=True,
