7e7ef7c14dae8ce1ed13147eb1d6ff031a711bfe
alex_str <alexstr22@gmail.com>
2019-10-20 11:35:40 +0300

    PAM stage 4 5

--- segmentation_models/models/fpn.py
+++ segmentation_models/models/fpn.py
@@ -101,7 +101,7 @@ def FPNBlock(pyramid_filters,segmentation_filters, stage):
             name=conv1_name,
         )(skip)
 
-        if stage == 5:
+        if (stage == 5) & stage == 4):
             # input_tensor
             pam = PAM()(input_tensor)
             pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
@@ -113,14 +113,14 @@ def FPNBlock(pyramid_filters,segmentation_filters, stage):
             input_tensor = pam
 
             # skip
-            pam = CAM()(skip)
-            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
-            pam = BatchNormalization(axis=3)(pam)
-            pam = Activation('relu')(pam)
-            pam = Dropout(0.5)(pam)
-            pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
-
-            skip = pam
+            # pam = PAM()(skip)
+            # pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            # pam = BatchNormalization(axis=3)(pam)
+            # pam = Activation('relu')(pam)
+            # pam = Dropout(0.5)(pam)
+            # pam = Conv2D(pyramid_filters, 3, padding='same', use_bias=False, kernel_initializer='he_normal')(pam)
+            #
+            # skip = pam
 
             # input_tensor = Dropout(0.5)(input_tensor)
             # input_tensor = Conv2d_BN(input_tensor, segmentation_filters, 1)
